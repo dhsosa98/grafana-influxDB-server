18cd9229304f804ac6a1732284b0c799f1c94f94
Diego <dhsosa98@gmail.com>
2022-12-08 14:03:08 -0300

    add dashboards

--- main.py
+++ main.py
@@ -180,4 +180,4 @@ async def influx():
         return {"message": e}
 
 if (__name__ == "__main__"):
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
